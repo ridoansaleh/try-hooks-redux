051b1c9f66bb96043421dc8af3aca9311cd9fd03
ridoansaleh <ridoansalehnasution98@gmail.com>
2019-06-13 20:57:18 +0700

    add state into App component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ import { connect } from "react-redux";
 import { fetchAllPost } from "./actions/postAction";
 
 class App extends Component {
+  state = {
+    isPrevBtnActive: true,
+    isNextBtnActive: false
+  };
+
   componentDidMount() {
     this.props.fetchAllPost();
   }
 
+  handlePrevBtn = () => {
+    this.setState({
+      isPrevBtnActive: true,
+      isNextBtnActive: false
+    });
+  };
+
+  handleNextBtn = () => {
+    this.setState({
+      isPrevBtnActive: false,
+      isNextBtnActive: true
+    });
+  };
+
   render() {
+    const { isNextBtnActive, isPrevBtnActive } = this.state;
     const { posts } = this.props;
     if (!posts.isFetched) {
       return (
@@ -19,11 +39,46 @@ class App extends Component {
     return (
       <div>
         {posts.data.length > 0 ? (
-          <ul>
-            {posts.data.map((post, i) => {
-              return <li key={i}>{post.title}</li>;
-            })}
-          </ul>
+          <div>
+            <div style={{ padding: 25 }}>
+              <h3>POSTS TITLE</h3>
+              {posts.data.map((post, i) => {
+                if (isPrevBtnActive && i <= 49) {
+                  return (
+                    <div>
+                      <p key={i}>
+                        {i + 1}. {post.title}
+                      </p>
+                    </div>
+                  );
+                }
+                if (isNextBtnActive && i > 49) {
+                  return (
+                    <div>
+                      <p key={i}>
+                        {i + 1}. {post.title}
+                      </p>
+                    </div>
+                  );
+                }
+                return null;
+              })}
+            </div>
+            <div style={{ textAlign: "center", marginBottom: 10 }}>
+              <button
+                style={{ display: "inline-block" }}
+                onClick={this.handlePrevBtn}
+              >
+                Prev
+              </button>
+              <button
+                style={{ display: "inline-block" }}
+                onClick={this.handleNextBtn}
+              >
+                Next
+              </button>
+            </div>
+          </div>
         ) : (
           <p>There is no post</p>
         )}
